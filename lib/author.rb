6459162148f7ba5b40d2048b15dde1f15ebc1837
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-05 02:05:57 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
 
     attr_accessor :name
 
+    @@post_coun = 0
+
     def initialize(name)
         @name = name
         @posts = []
@@ -15,5 +17,9 @@ class Author
     def add_post_by_title(title)
         post = Post.new(title)
         self.add_post(post)
+    end
+
+    def self.post_count
+
 
 end
