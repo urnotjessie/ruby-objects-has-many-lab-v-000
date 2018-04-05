fa6948c3b52736d8332f06fd0d4568e42dda750b
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-05 02:09:37 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+
+    attr_accessor :title, :author
+
+    def initialize(title)
+        @title = title
+    end
+
+    def author_name
+        if self.author != nil
+            self.author.name
+        else
+            nil
+        end
+    end
+
+
+end
